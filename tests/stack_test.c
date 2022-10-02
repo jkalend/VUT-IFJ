5a75603df21db5adbb5253fadea820b2a57d8402
Jan.Kalenda <jan.kalenda@solarwinds.com>
2022-10-02 02:53:11 +0200

    increased coverage?
    Signed-off-by: Jan.Kalenda <jan.kalenda@solarwinds.com>

--- tests/stack_test.c
+++ tests/stack_test.c
@@ -8,39 +8,96 @@
 
 int test1(TStack*S, TData *a, TData *b, TData *c) {
     stack_init(S);
-    assert(stack_isEmpty(S));
+    //assert(stack_isEmpty(S));
+    if (!stack_isEmpty(S)) {
+        return 1;
+    }
     stack_push(S, a);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == a);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == a);
+    if (stack_top(S) != a) {
+        return 1;
+    }
     stack_push(S, b);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == b);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == b);
+    if (stack_top(S) != b) {
+        return 1;
+    }
     stack_push(S, c);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == c);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == c);
+    if (stack_top(S) != c) {
+        return 1;
+    }
     return 0;
 }
 
 int test2(TStack*S, TData *a, TData *b, TData *c) {
     stack_init(S);
-    assert(stack_isEmpty(S));
+    //assert(stack_isEmpty(S));
+    if (!stack_isEmpty(S)) {
+        return 1;
+    }
     stack_push(S, a);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == a);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == a);
+    if (stack_top(S) != a) {
+        return 1;
+    }
     stack_push(S, b);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == b);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == b);
+    if (stack_top(S) != b) {
+        return 1;
+    }
     stack_push(S, c);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == c);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == c);
+    if (stack_top(S) != c) {
+        return 1;
+    }
     stack_pop(S);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == b);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == b);
+    if (stack_top(S) != b) {
+        return 1;
+    }
     stack_pop(S);
-    assert(!stack_isEmpty(S));
-    assert(stack_top(S) == a);
+    //assert(!stack_isEmpty(S));
+    if (stack_isEmpty(S)) {
+        return 1;
+    }
+    //assert(stack_top(S) == a);
+    if (stack_top(S) != a) {
+        return 1;
+    }
     stack_pop(S);
-    assert(stack_isEmpty(S));
+    //assert(stack_isEmpty(S));
+    if (!stack_isEmpty(S)) {
+        return 1;
+    }
     return 0;
 }
 
